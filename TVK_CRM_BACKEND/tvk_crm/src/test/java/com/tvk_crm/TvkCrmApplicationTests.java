dbfbf7d712d82c62d9d021d29e2152e2bb84248c
Nalla Chandana <chandana193@gmail.com>
2026-07-14 16:34:56 +0530

    added the backend and frontend folders

---
+++ TVK_CRM_BACKEND/tvk_crm/src/test/java/com/tvk_crm/TvkCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvk_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvkCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
