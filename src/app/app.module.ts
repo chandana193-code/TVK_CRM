bb832b4d89d16abb7e540652f323057681304561
Nalla Chandana <chandana193@gmail.com>
2026-07-15 09:54:29 +0530

    registration created

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StudentRegistrationComponent } from './components/student-registration/student-registration.component';
+import { TrainersComponent } from './components/trainers/trainers.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    NavbarComponent,
+    StudentRegistrationComponent,
+    TrainersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
